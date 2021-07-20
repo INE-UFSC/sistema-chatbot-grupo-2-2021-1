5002c21c59a08c16b4ffd54ddc686fc9d4d6a5ac
Tulio Bardini <tulio.bardini53@gmail.com>
2021-07-20 11:28:50 -0300

    Sistema chat Bot

--- SistemaChatBot/SistemaChatBot.py
+++ SistemaChatBot/SistemaChatBot.py
@@ -3,35 +3,42 @@ from Bots.Bot import Bot
 class SistemaChatBot:
     def __init__(self,nomeEmpresa,lista_bots):
         self.__empresa=nomeEmpresa
-        ##verificar se a lista de bots contém apenas bots
         self.__lista_bots=lista_bots
         self.__bot = None
     
     def boas_vindas(self):
-        pass
-        ##mostra mensagem de boas vindas do sistema
+        
+        return "Olá, esse é o sistema de chatbots da empresa " + self.__empresa 
 
     def mostra_menu(self):
-        pass
-        ##mostra o menu de escolha de bots
+       
+        return self.__lista_bots
     
-    def escolhe_bot(self):
-        pass
+    def escolhe_bot(self, numero):
+        self.__bot = self.__lista_bots[numero]
+        return self.__bot.boas_vindas()
         ##faz a entrada de dados do usuário e atribui o objeto ao atributo __bot 
 
     def mostra_comandos_bot(self):
-        pass
+        return self.__bot.mostra_comandos()
         ##mostra os comandos disponíveis no bot escolhido
 
-    def le_envia_comando(self):
-        pass
+    def le_envia_comando(self, comando):
+
+        return self.__bot.executa_comando(comando)         
         ##faz a entrada de dados do usuário e executa o comando no bot ativo
 
     def inicio(self):
-        pass
-        ##mostra mensagem de boas-vindas do sistema
-        ##mostra o menu ao usuário
-        ##escolha do bot      
-        ##mostra mensagens de boas-vindas do bot escolhido
-        ##entra no loop de mostrar comandos do bot e escolher comando do bot até o usuário definir a saída
-        ##ao sair mostrar a mensagem de despedida do bot
+        sistema = SistemaChatBot()
+        sistema.boas_vindas()
+        sistema.mostra_menu()
+        sistema.escolhe_bot("Digite o número do chat bot desejado: ")
+        sistema.mostra_comandos_bot()
+        while True:
+            sistema.mostra_comandos_bot()
+            sistema.le_envia_comando("Digite o comando desejado (ou -1 fechar o programa e sair): ")
+            sistema.boas_vindas()
+            sistema.mostra_menu()
+            sistema.escolhe_bot("Digite o número do chat bot desejado: ")
+            sistema.mostra_comandos_bot()    
+        
