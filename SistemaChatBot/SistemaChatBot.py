e15a40d1c67d8f3139d4d3c4162498f8f71fb41f
Tulio Bardini <tulio.bardini53@gmail.com>
2021-07-22 14:57:18 -0300

    Acredito que agora o SistemaChatBot esta operando normalmente, contudo o meu VsCode esta dando algum erro relacionado a importação dos outros Bots, então não consegui testar o código em sua totalidade

--- SistemaChatBot/SistemaChatBot.py
+++ SistemaChatBot/SistemaChatBot.py
@@ -1,44 +1,52 @@
 from Bots.Bot import Bot
 
+
 class SistemaChatBot:
     def __init__(self,nome_empresa,lista_bots):
         self.__empresa=nome_empresa
-        self.__lista_bots=lista_bots
         self.__bot = None
-    
+        self.__lista_bots = []
+        for bot in lista_bots:
+            if type(bot) == Bot:
+                self.__lista_bots.append(bot)
+
     def boas_vindas(self):
         
-        return "Olá, esse é o sistema de chatbots da empresa " + self.__empresa 
+        print("Olá, esse é o sistema de chatbots da empresa " + self.__empresa) 
 
     def mostra_menu(self):
-       
-        return self.__lista_bots
+        
+        for index, bot in enumerate(self.__lista_bots):
+            print(str(index)+" - Bot: " + bot.nome + " - Mensagem de apresentação: " 
+            + bot.apresentacao())
     
     def escolhe_bot(self, numero):
         self.__bot = self.__lista_bots[numero]
-        return self.__bot.boas_vindas()
+        print(self.__bot.boas_vindas())
         ##faz a entrada de dados do usuário e atribui o objeto ao atributo __bot 
 
     def mostra_comandos_bot(self):
-        return self.__bot.mostra_comandos()
+
+        print(self.__bot.mostra_comandos())
         ##mostra os comandos disponíveis no bot escolhido
 
     def le_envia_comando(self, comando):
 
-        return self.__bot.executa_comando(comando)         
+        print("--> "+self.__bot.nome +" diz: " + self.__bot.comandos[comando])
+        print(" --> Eu te respondo:" + self.__bot.executa_comando(comando))         
         ##faz a entrada de dados do usuário e executa o comando no bot ativo
 
     def inicio(self):
-        sistema = SistemaChatBot()
-        sistema.boas_vindas()
-        sistema.mostra_menu()
-        sistema.escolhe_bot("Digite o número do chat bot desejado: ")
-        sistema.mostra_comandos_bot()
+        self.boas_vindas()
+        self.mostra_menu()
+        escolhaBot = input("Digite o número do chat bot desejado: ")
+        self.escolhe_bot(escolhaBot)
+        self.mostra_comandos_bot()
         while True:
-            sistema.mostra_comandos_bot()
-            sistema.le_envia_comando("Digite o comando desejado (ou -1 fechar o programa e sair): ")
-            sistema.boas_vindas()
-            sistema.mostra_menu()
-            sistema.escolhe_bot("Digite o número do chat bot desejado: ")
-            sistema.mostra_comandos_bot()    
+            escolhaComando = input("Digite o comando desejado (ou -1 fechar o programa e sair): ")
+            if escolhaComando == "-1":
+                print(self.le_envia_comando("4"))
+                break
+            self.le_envia_comando(escolhaComando)
+            self.mostra_comandos_bot()    
         
