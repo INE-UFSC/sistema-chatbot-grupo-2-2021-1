d94906ce3efb7f17874213ed8a18ab54e454e62e
Patrícia <patriciab.arigoni17@gmail.com>
2021-07-22 18:00:41 -0300

    ajustes para possibilitar o funcionamento do SistemaChatBot

--- Bots/BotZangado.py
+++ Bots/BotZangado.py
@@ -1,17 +1,26 @@
 from Bots.Bot import Bot
 
 class BotZangado(Bot):
+    comandos = {"1": "Bom dia", 
+                "2": "Qual o seu nome?",
+                "3": "Quero um conselho",
+                "4": "Adeus"}
+    
     def __init__(self, nome: str):
         super().__init__(nome)
 
     def apresentacao(self) -> str:
-        return ('Aff, estão me perturbando de novo! Eu sou o %s >:@' % self.__nome)
+        return ("Aff, estão me perturbando de novo! Eu sou o %s >:@" % self.nome)
     
     def executa_comando(self, cmd: str) -> str:
-        return self.__comandos[cmd]
+        respostas = {"1": "Bom dia? Bom dia pra quem??!!",
+                     "2": ("É %s!!! Você tem uma memória péssima assim sempre?" % self.nome),
+                     "3": "Hmmm... que tal parar de incomodar os outros? Esse é o meu conselho.",
+                     "4": "Poderia dizer que foi um prazer, mas estaria mentindo."}
+        return respostas[cmd]
 
     def boas_vindas(self) -> str:
-        return 'Meu deus, eu não tenho um dia de paz!!'
+        return "Meu deus, eu não tenho um dia de paz!!"
 
     def despedida(self) -> str:
-        return 'ALELUIA!!! Não aguentava mais.'
+        return "ALELUIA!!! Não aguentava mais."
--- SistemaChatBot/SistemaChatBot.py
+++ SistemaChatBot/SistemaChatBot.py
@@ -7,46 +7,44 @@ class SistemaChatBot:
         self.__bot = None
         self.__lista_bots = []
         for bot in lista_bots:
-            if type(bot) == Bot:
+            if isinstance(bot, Bot):
                 self.__lista_bots.append(bot)
 
     def boas_vindas(self):
-        
-        print("Olá, esse é o sistema de chatbots da empresa " + self.__empresa) 
+        print("Olá, esse é o sistema de chatbots da empresa %s\n" % self.__empresa)
 
     def mostra_menu(self):
-        
+        print('Os chat bots disponíveis no momento são: ')
         for index, bot in enumerate(self.__lista_bots):
-            print(str(index)+" - Bot: " + bot.nome + " - Mensagem de apresentação: " 
+            print(str(index) + " - Bot: " + bot.nome + " - Mensagem de apresentação: " 
             + bot.apresentacao())
+        print()
     
     def escolhe_bot(self, numero):
         self.__bot = self.__lista_bots[numero]
-        print(self.__bot.boas_vindas())
+        print("--> " + self.__bot.nome + " diz " + self.__bot.boas_vindas())
         ##faz a entrada de dados do usuário e atribui o objeto ao atributo __bot 
 
     def mostra_comandos_bot(self):
-
-        print(self.__bot.mostra_comandos())
+        return self.__bot.mostra_comandos()
         ##mostra os comandos disponíveis no bot escolhido
 
     def le_envia_comando(self, comando):
-
-        print("--> "+self.__bot.nome +" diz: " + self.__bot.comandos[comando])
-        print(" --> Eu te respondo:" + self.__bot.executa_comando(comando))         
+        print("--> " + self.__bot.nome + " diz: Você disse " + self.__bot.comandos[comando])
+        print(" --> Eu te respondo: " + self.__bot.executa_comando(comando))         
         ##faz a entrada de dados do usuário e executa o comando no bot ativo
 
     def inicio(self):
         self.boas_vindas()
         self.mostra_menu()
-        escolhaBot = input("Digite o número do chat bot desejado: ")
+        escolhaBot = int(input("Digite o número do chat bot desejado: "))
         self.escolhe_bot(escolhaBot)
-        self.mostra_comandos_bot()
         while True:
+            self.mostra_comandos_bot()
+            print()
             escolhaComando = input("Digite o comando desejado (ou -1 fechar o programa e sair): ")
+            print()
             if escolhaComando == "-1":
-                print(self.le_envia_comando("4"))
+                print("--> " + self.__bot.nome + " diz " + self.__bot.despedida())
                 break
-            self.le_envia_comando(escolhaComando)
-            self.mostra_comandos_bot()    
-        
+            self.le_envia_comando(escolhaComando) 
